a09339ddaf7d938a972e486294bb578ce66ed048
lucastheldl <lucastheldl@gmail.com>
2023-04-04 16:13:21 -0300

    Added more responsivity to home page

--- src/pages/Home/components/Countdown/styles.ts
+++ src/pages/Home/components/Countdown/styles.ts
@@ -5,14 +5,30 @@ export const CountdownContainer = styled.div`
   font-size: 10rem;
   line-height: 8rem;
   color: ${(props) => props.theme['gray-100']};
-
   display: flex;
   gap: 1rem;
-
   span {
     background: ${(props) => props.theme['gray-700']};
     padding: 2rem 1rem;
     border-radius: 8px;
+  }
+  @media (max-width: 650px) {
+    font-size: 6rem;
+    line-height: 5rem;
+    gap: 0.5rem;
+  }
+  @media (max-width: 460px) {
+    font-size: 4rem;
+    line-height: 3rem;
+    gap: 0.3rem;
+  }
+  @media (max-width: 320px) {
+    font-size: 3rem;
+    line-height: 2rem;
+    gap: 0.2rem;
+    span {
+      padding: 1rem 0.5rem;
+    }
   }
 `
 export const Separator = styled.div`
@@ -22,4 +38,10 @@ export const Separator = styled.div`
   overflow: hidden;
   display: flex;
   justify-content: center;
+  @media (max-width: 460px) {
+    width: 3rem;
+  }
+  @media (max-width: 320px) {
+    width: 2rem;
+  }
 `
